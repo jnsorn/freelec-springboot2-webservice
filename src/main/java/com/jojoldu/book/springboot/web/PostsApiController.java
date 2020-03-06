b15209355a59bc98571d76c0b02888120ba9fed6
soring <jnsorn@gmail.com>
2020-03-06 19:05:50 +0900

    feat : Posts 수정 기능 구현

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -1,10 +1,13 @@
 package com.jojoldu.book.springboot.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jojoldu.book.springboot.service.posts.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 
@@ -17,5 +20,11 @@ public class PostsApiController {
 	public Long save(@RequestBody PostsSaveRequestDto requestDto){
 		return postsService.save(requestDto);
 	}
+
+	@GetMapping("/api/v1/posts/{id}")
+	public PostsResponseDto findById(@PathVariable Long id){
+		return postsService.findById(id);
+	}
+
 
 }

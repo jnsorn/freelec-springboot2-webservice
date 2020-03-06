9e36933e7c7d7a9d7dcf53229752ab2a3806b991
soring <jnsorn@gmail.com>
2020-03-06 19:56:06 +0900

    feat : BaseTimeEntity 등록

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -2,6 +2,8 @@ package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.jojoldu.book.springboot.domain.posts.Posts;
+
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
 }

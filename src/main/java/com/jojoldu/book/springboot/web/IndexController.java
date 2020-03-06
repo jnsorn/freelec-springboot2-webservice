50a346e2ba0cac79cfd807972adf395ba69eb3c7
soring <jnsorn@gmail.com>
2020-03-06 21:39:42 +0900

    feat : Mustache를 이용하여 글 등록하는 기능 구현

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -2,11 +2,16 @@ package com.jojoldu.book.springboot.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+/* NOTE : 페이지와 관련된 컨트롤러*/
 @Controller
 public class IndexController {
 	@GetMapping("/")
 	public String index(){
 		return "index";
+	}
+
+	@GetMapping("/posts/save")
+	public String postsSave(){
+		return "posts-save";
 	}
 }
